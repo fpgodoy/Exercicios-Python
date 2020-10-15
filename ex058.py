3000e2961b6c24549b3df5ebb64e2121fa7efbc4
Fellipe Godoy <65260070+fpgodoy@users.noreply.github.com>
2020-10-14 21:13:07 -0400

    Corrigido exercício 58
    Acrescentada uma dica (maior ou menor) para adivinhar o número.

--- ex058.py
+++ ex058.py
@@ -3,6 +3,10 @@ num = randint(0,10)
 q = 1
 chute = int(input ('O computador escolheu um número entre 0 e 10. Tente adivinhar qual foi: '))
 while chute != num:
-    chute = int(input ('Vocẽ errou, tente novamente. Escolha outro número: '))
+    if chute > num:
+        comparado = 'menor'
+    elif chute < num:
+        comparado = 'maior'
+    chute = int(input ('Você errou, o número é {} que esse. Escolha outro número: '.format(comparado)))
     q += 1
 print('Parabéns, você acertou, o número era {}. Você precisou de {} tentativas para acertar.'.format(num, q))
