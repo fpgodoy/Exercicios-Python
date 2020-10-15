6446af2b42b0b72ee9195fd4b6dd20bfa7dc25db
Fellipe Godoy <65260070+fpgodoy@users.noreply.github.com>
2020-10-14 21:05:25 -0400

    Corridigo exercício 57

--- ex057.py
+++ ex057.py
@@ -1,4 +1,4 @@
-s = str(input('Digite o sexo da pessoa [F/M] ')).upper()
+s = str(input('Digite o sexo da pessoa [F/M] ')).strip().upper()[0]
 while s != 'F' and s != 'M':
     s = str(input('Digite F para feminino ou M para masculino: ')).upper()
 if s == 'F':
