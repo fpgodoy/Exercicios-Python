8e53d82e18608199116ed9c24397179e74a8e3e8
Fellipe Godoy <65260070+fpgodoy@users.noreply.github.com>
2020-10-16 20:26:54 -0400

    Correção do exercício 61
    Corrigindo typos e simplificando as ,s e .s na lista de termos.

--- ex061.py
+++ ex061.py
@@ -1,10 +1,10 @@
-print('Calculadora de prograssão aritimética.\nImportante: este programam mostra os primeiros 10 termos da progressão.')
+print('Calculadora de progressão aritmética.\nImportante: este programa mostra os 10 primeiros termos da progressão.')
 p = int(input('Digite o primeiro termo: '))
 r = int(input('Digite a razão: '))
 c = 1
-print('Os primeiros 10 termos da PA apresentada são', end=" ")
-while c < 10:
+print('Os 10 primeiros termos da PA apresentada são ', end="")
+while c <= 10:
+    print(p,', ' if c < 10 else '.', end = "")
     c += 1
-    p = p + r
-    print(p, end=", ")
-print('{}.'.format(p + r))
+    p += r
+print('\n')
