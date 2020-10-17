b6a0bc1330697994674f172c71a22de6b146be0f
Fellipe Godoy <65260070+fpgodoy@users.noreply.github.com>
2020-10-16 20:52:52 -0400

    Corrigido exercício 64
    Melhorado o código, trazendo o input do flag pra fora do while, desconsiderando o flag na contagem e na soma.

--- ex064.py
+++ ex064.py
@@ -1,14 +1,12 @@
-n = 0
-c = -1
-s = n
+s = c = 0
 print('=-'*21, 'SOMADORA', '-='*21)
 print('Você pode somar quantos números quiser. Quando quiser encerrar e saber o resultado, digite 999.')
 print('-'*94)
+n = int(input('Digite um número inteiro:'))
 while n != 999:
-    a = s
     c +=1
+    s += n
     n = int(input('Digite um número inteiro:'))
-    s = a + n
 print('='*94)
-print('Quantidade de números digitados: {}\nSoma dos números digitados: {}'.format(c, s - 999))
+print('Quantidade de números digitados: {}\nSoma dos números digitados: {}'.format(c, s))
 print('='*94)
