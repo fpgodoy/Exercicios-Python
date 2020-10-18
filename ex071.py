e6b899adbf0a6014eca774f172f2d564d079185c
Fellipe Godoy <65260070+fpgodoy@users.noreply.github.com>
2020-10-18 11:39:03 -0400

    Corrigido exercício 71
    Otimizado o código, utlizando menos variáveis e menos breaks.

--- ex071.py
+++ ex071.py
@@ -1,29 +1,21 @@
 print('=' * 30)
-print('CAIXA ELETRÔNICO')
+print('{:^30}'.format('CAIXA ELETRÔNICO'))
 print('=' * 30)
 valor = int(input('Qual o valor do saque? R$'))
+ced = 50
 while True:
-    cinq = vinte = dez = um = 0
-    cinq = int(valor / 50)
-    if cinq > 0:
-        print(f'Total de {cinq} cédulas de R$50')
-    if valor % 50 == 0:
+    qced = int(valor / ced)
+    if qced > 0:
+        print(f'Total de {qced} cédulas de R${ced}')
+    if valor % ced == 0:
         break
-    valor = valor % 50
-    vinte = int(valor / 20)
-    if vinte > 0:
-        print(f'Total de {vinte} cédulas de R$20')
-    if valor % 20 == 0:
-        break
-    valor = valor % 20
-    dez = int(valor / 10)
-    if dez > 0:
-        print(f'Total de {dez} cédulas de R$10')
-    if valor % 10 == 0:
-        break
-    valor = valor % 10
-    um = valor
-    print(f'Total de {um} cédulas de R$1')
-    break
+    else:
+        valor = valor % ced
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1    
 print('=' * 30)
 print('Obrigado e volte sempre.')
