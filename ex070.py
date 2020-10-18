280e72f6266ba8bab696e7037676cabe5656844d
Fellipe Godoy <65260070+fpgodoy@users.noreply.github.com>
2020-10-18 11:23:28 -0400

    Corrigindo exercício 70
    Centralizado texto do título do programa. Melhorada a validação da resposta do input S/N. Formatadas casas decimais do preço do produto mais barato.

--- ex070.py
+++ ex070.py
@@ -1,7 +1,7 @@
 total = mais = pricebarato = 0
 barato = ''
 print('-' * 40)
-print('LOJA SUPER BARATÃO')
+print('{:^40}'.format('LOJA SUPER BARATÃO'))
 print('-' * 40)
 while True:
     nome = str(input('Nome do Produto: '))
@@ -14,9 +14,9 @@ while True:
     if price <= pricebarato:
         barato = nome
         pricebarato = price
-    cont = str(input('Deseja continuar? [S/N] ')).upper().strip()
-    while cont != 'S' and cont != 'N':
-        cont = str(input('Deseja continuar? [S/N] ')).upper().strip()
+    cont = ' '
+    while cont not in 'SN':
+        cont = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
     if cont == 'N':
         break
 print('-' * 40)
@@ -24,4 +24,4 @@ print('Fechando sua compra...')
 print('-' * 40)
 print(f'Total gasto na compra: R${total:.2f}')
 print(f'Produtos que custaram mais de R$1000.00: {mais}')
-print(f'Produto mais barato: {barato}, que custou R${pricebarato}')
+print(f'Produto mais barato: {barato}, que custou R${pricebarato:.2f}')
