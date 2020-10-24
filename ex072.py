b3c115188b74b9e270ec45e57e85abdc9b1891ed
Fellipe Godoy <65260070+fpgodoy@users.noreply.github.com>
2020-10-24 10:45:31 -0400

    Resolvido exercício 72

---
+++ ex072.py
@@ -0,0 +1,7 @@
+ext = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    n = int(input('Digite um número entre 0 e 20: '))
+    if 0 < n < 20:
+        break
+    print('Tente novamente.')
+print(f'Você digitou o número {ext[n]}.')
