37c8e94cb02a26ce0028de43febf1d15a31033db
Fellipe Godoy <65260070+fpgodoy@users.noreply.github.com>
2020-10-29 15:41:59 -0400

    Corrigido exercício 73
    Diminuída a quantidade de colunas e utilização de f string em todos os print, utilizando aspas duplas na função de index.

--- ex073.py
+++ ex073.py
@@ -1,5 +1,12 @@
-times = ('Internacional', 'Flamengo', 'Atlético-MG', 'São Paulo', 'Santos', 'Fluminense', 'Fortaleza', 'Palmeiras', 'Atlético-GO', 'Corinthians', 'Grêmio', 'Sport Recife', 'Bahia', 'Ceará SC', 'Botafogo', 'Vasco da Gama', 'Athletico-PR', 'Coritiba', 'Bragantino-SP', 'Goiás')
+times = ('Internacional', 'Flamengo', 'Atlético-MG', 'São Paulo', 'Santos',
+'Fluminense', 'Fortaleza', 'Palmeiras', 'Atlético-GO', 'Corinthians',
+'Grêmio', 'Sport Recife', 'Bahia', 'Ceará SC', 'Botafogo', 'Vasco da Gama',
+'Athletico-PR', 'Coritiba', 'Bragantino-SP', 'Goiás')
+print('-='*15)
 print(f'Os 5 primeiros colocados no Campeonato Brasileiro são {times[:5]}.')
+print('-='*15)
 print(f'Os últimos 4 colocados são {times[-4:]}.')
+print('-='*15)
 print(f'Os times que estão competindo, em ordem alfabética, são {sorted(times)}.')
-print('O time do Coritiba está em {}ª posição.'.format(times.index('Coritiba') + 1))
+print('-='*15)
+print(f'O time do Coritiba está em {times.index("Coritiba") + 1}ª posição.')
