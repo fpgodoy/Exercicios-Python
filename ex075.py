b5a8c1b391c649d03f927ad92fc0f96c5b1ce929
Fellipe Godoy <65260070+fpgodoy@users.noreply.github.com>
2020-10-29 15:56:22 -0400

    Corrigido exercício 75
    Limitada quantidade de colunas e utilizada f string nos prints

--- ex075.py
+++ ex075.py
@@ -1,11 +1,12 @@
-valores = (int(input('Digite um número: ')), int(input('Digite um número: ')), int(input('Digite um número: ')), int(input('Digite um número: ')))
+valores = (int(input('Digite um número: ')), int(input('Digite um número: ')),
+int(input('Digite um número: ')), int(input('Digite um número: ')))
 print(f'Você digitou os valores {valores}.')
-print('O número 9 apareceu {} vezes.'.format(valores.count(9)))
+print(f'O número 9 apareceu {valores.count("9")} vezes.')
 if 3 not in valores: 
     print('Não foi digitado o número 3.')
 else:
-    print('O número 3 foi digitado na {}ª posição.'.format(valores.index(3) + 1))
-print('Os números pares foram: ')
+    print(f'O número 3 foi digitado na {valores.index(3) + 1}ª posição.')
+print('Os números pares foram: ', end = ' ')
 for c in valores:
     if c % 2 == 0:
         print(c, end = " ")
