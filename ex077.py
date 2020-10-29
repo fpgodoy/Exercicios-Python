a80cb0ac3381056118bc67678bdccf69465c6c79
Fellipe Godoy <65260070+fpgodoy@users.noreply.github.com>
2020-10-29 16:10:53 -0400

    Corrigido exercício 77
    Utilizada f string no print

--- ex077.py
+++ ex077.py
@@ -1,6 +1,6 @@
 palavras = ('banana', 'maca', 'pera', 'melao', 'melancia', 'coco')
 for word in palavras:
-    print('A palavra \33[35m{}\33[m possui as vogais:'.format(word.upper()), end = "")
+    print(f'A palavra \33[35m{word.upper()}\33[m possui as vogais:', end = "")
     for letras in word:
         if letras in 'aeiou':
             print('\33[33:1m ', letras.upper(), end = "")
