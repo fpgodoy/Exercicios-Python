393e8236984710b4db62e269e48629c7aefc01d1
Fellipe Godoy <65260070+fpgodoy@users.noreply.github.com>
2020-10-29 15:34:24 -0400

    Correção exercício 72
    código com menos de 80 colunas e incluída repetição

--- ex072.py
+++ ex072.py
@@ -1,7 +1,15 @@
-ext = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-while True:
-    n = int(input('Digite um número entre 0 e 20: '))
-    if 0 < n < 20:
-        break
-    print('Tente novamente.')
-print(f'Você digitou o número {ext[n]}.')
+ext = ('zero', 'um', 'dois', 'três', 'quatro',
+'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
+'onze', 'doze', 'treze', 'quatorze', 'quinze',
+'dezesseis', 'dezessete', 'dezoito', 'dezenove',
+'vinte')
+rep = 's'
+while rep == 's':
+    while True:
+        n = int(input('Digite um número entre 0 e 20: '))
+        if 0 <= n <= 20:
+            break
+        print('Tente novamente.')
+    print(f'Você digitou o número {ext[n]}.')
+    rep = str(input('Quer continuar? [S/N] '))
+print('Programa finalizado.')
