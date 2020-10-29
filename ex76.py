8ad7ccba30da194adcc8508f0897c55db695161c
Fellipe Godoy <65260070+fpgodoy@users.noreply.github.com>
2020-10-29 16:06:07 -0400

    Corrigido exerício 76
    Utilizando f string nos prints

--- ex76.py
+++ ex76.py
@@ -1,8 +1,8 @@
 compras = ('Lápis', 100.00, 'Caderno', 5.25, 'Tomate', 8.72, 'Cebola', 1)
 print('-' * 59)
-print('\33[7mLISTAGEM DE PREÇOS\33[m'.center(59))
+print(f'\33[7m{"LISTAGEM DE PREÇOS":^59}\33[m')
 print('-' * 59)
 for produto in range(0, len(compras), 2):
-    print('{}'.format(compras[produto]).ljust(50, "."), end = "")
-    print('\33[31mR${:7.2f}\33[m'.format(compras[produto + 1]))
+    print(f'{compras[produto]:.<50}', end = "")
+    print(f'\33[31mR${compras[produto + 1]:>7.2f}\33[m')
 print('-' * 59)
