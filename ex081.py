3553f23bc3d2c358b613e4386d73c5dea6316a18
Fellipe Godoy <65260070+fpgodoy@users.noreply.github.com>
2020-11-21 18:27:08 -0400

    Corrigido exercpicio 81
    acrescentado o tipo nos inputs para permitir ordenar os valores da lita

--- ex081.py
+++ ex081.py
@@ -1,13 +1,13 @@
 valores = list()
 while True:
-    valores.append(input('Digite um valor: '))
-    repeat = input('Deseja informar mais valores? [S/N] ')
+    valores.append(int(input('Digite um valor: ')))
+    repeat = str(input('Deseja informar mais valores? [S/N] '))
     if repeat in 'Nn':
         break
 print('-='*10)
 print(f'Foram digitados {len(valores)} números.')
 valores.sort(reverse=True)
-print(f'Os número digitados foram {valores}')
+print(f'Os número digitados, em ordem decrescente, são {valores}')
 if '5' in valores:
     print('O valor 5 está na lista.')
 else:
